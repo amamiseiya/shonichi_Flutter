1fd78932aefc76a93e90e70f03af68c21ff3b9da
天海星夜 <knifemule@gmail.com>
2020-06-30 17:08:41 +0800

    Initial Commit

---
+++ shonichi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shonichi
+//
+//  Created by 黄彦璋 on 2020/6/30.
+//  Copyright © 2020 seiya studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
